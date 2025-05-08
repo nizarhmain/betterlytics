65eb408bbe2ed4f134ea1525e78db747a7fba5c5
jepli <jepli@itu.dk>
2025-05-08 20:04:37 +0200

    Made referrer columns default empty string instead of nullable and renamed migration to correct increment

--- migrations/5_add_referrer_source.sql
+++ migrations/5_add_referrer_source.sql
@@ -1,8 +1,8 @@
 ALTER TABLE analytics.events
     ADD COLUMN IF NOT EXISTS referrer_source String DEFAULT 'direct',
-    ADD COLUMN IF NOT EXISTS referrer_source_name Nullable(String),
-    ADD COLUMN IF NOT EXISTS referrer_search_term Nullable(String),
-    ADD COLUMN IF NOT EXISTS referrer_url Nullable(String);
+    ADD COLUMN IF NOT EXISTS referrer_source_name String DEFAULT '',
+    ADD COLUMN IF NOT EXISTS referrer_search_term String DEFAULT '',
+    ADD COLUMN IF NOT EXISTS referrer_url String DEFAULT '';
 
 ALTER TABLE analytics.events
     ADD INDEX referrer_source_idx referrer_source TYPE bloom_filter GRANULARITY 3;
