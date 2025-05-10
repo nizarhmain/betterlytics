d959d05675a7d1db1d90ce7159fd6b2f76ff048d
Thomas <thomas.schauser@gmail.com>
2025-05-10 11:21:50 +0200

    Added hook for reusable fragmented time series

---
+++ better-analytics-dashboard/src/hooks/useFragmentedGranularityTimeSeriesLineChart.ts
@@ -0,0 +1,96 @@
+import { GranularityRangeValues } from "@/utils/granularityRanges";
+import { scaleTime } from "d3-scale";
+import { utcDay, utcHour, utcMinute } from "d3-time";
+import { timeFormat } from "d3-time-format";
+import { useMemo } from "react";
+
+type FragmentedGranularityTimeSeriesLineChartProps<K extends string> = {
+  dataKey: K,
+  data: Array<{date: string} & Record<K, number>>,
+  granularity: GranularityRangeValues
+}
+
+export function useFragmentedGranularityTimeSeriesLineChart<K extends string>({ dataKey, data, granularity }: FragmentedGranularityTimeSeriesLineChartProps<K>) {
+
+  // Map date to value
+  const groupedData = useMemo(() => {
+    return data.reduce(
+      (group, row) => {
+        const key = new Date(row.date).valueOf().toString();
+        return { ...group, [key]: row[dataKey] }
+      }, {} as Record<string, number>
+    )
+  }, [data, dataKey]);
+
+  // Calculates upper and lower bound of time and values
+  const timeSeries = useMemo(() => {
+    const dates = Object.keys(groupedData).map((date) => +date);
+    const values = Object.values(groupedData);
+    return {
+        minTime: Math.min(...dates),
+        maxTime: Math.max(...dates),
+        minValue: Math.min(...values),
+        maxValue: Math.max(...values)
+      }
+    },
+    [groupedData]
+  );
+
+  // Fill in the missing dates
+  const chartData = useMemo(() => {
+    // Find the time interval of input based on specified granularity
+    const intervalFunc ={
+      'day': utcDay,
+      'hour': utcHour,
+      'minute': utcMinute,
+    }[granularity];
+
+    const filled = [];
+    // Iterate through each potential time frame
+    for (
+      let time = new Date(timeSeries.minTime);
+          time <= new Date(timeSeries.maxTime);
+          time = intervalFunc.offset(time, 1)
+    ) {
+      const key = time.valueOf().toString();
+      // Add entry - either with data from group or default value of 0
+      filled.push({
+        date: +key,
+        [dataKey]: groupedData[key] ?? 0
+      });
+    }
+    return filled;
+  }, [granularity, groupedData, timeSeries, dataKey]);
+
+  // Render tooltip based on granularity
+  const tooltipLabelFormatter = useMemo(() => {
+    return {
+      'day': timeFormat("%B %d, %Y"),
+      'hour': timeFormat("%H:%M, %B %d, %Y"),
+      'minute': timeFormat("%H:%M, %B %d, %Y"),
+    }[granularity];
+  }, [granularity])
+
+  // Generate scale for graph
+  const scale = useMemo(
+    () => scaleTime(
+      [timeSeries.minTime, timeSeries.maxTime],
+      [timeSeries.minValue, timeSeries.maxValue]
+    ),
+    [timeSeries]
+  );
+
+  // Generate list of "ticks"/labels to be added on the x-axis 
+  const ticks = useMemo(() => {
+    return scale.ticks(
+      utcDay.every(1)!
+    ) as unknown as number[]; // Recharts has incorrectly typed "ticks"
+  }, [scale]);
+
+  return {
+    chartData,
+    tooltipLabelFormatter,
+    scale,
+    ticks
+  };
+}
