d69d1eea42dc522409d0460ca84e58979d523073
Thomas <thomas.schauser@gmail.com>
2025-05-10 12:09:35 +0200

    Added migration

---
+++ migrations/7_add_custom_events_columns.sql
@@ -0,0 +1,9 @@
+ALTER TABLE analytics.events
+    ADD COLUMN IF NOT EXISTS event_type Enum('pageview' = 1, 'custom' = 2),
+    ADD COLUMN IF NOT EXISTS custom_event_name String DEFAULT '',
+    ADD COLUMN IF NOT EXISTS custom_event_json String DEFAULT '{}';
+
+ALTER TABLE analytics.events
+    ADD INDEX event_type_idx event_type TYPE bloom_filter GRANULARITY 3,
+    ADD INDEX custom_event_name_idx custom_event_name TYPE bloom_filter GRANULARITY 3;
+ 
