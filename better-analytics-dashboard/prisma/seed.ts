97cb210ff7b9d4a14c5ed5f85bdf045f47caef94
Thomas <thomas.schauser@gmail.com>
2025-05-11 18:34:10 +0200

    Added seeding for funnel

--- better-analytics-dashboard/prisma/seed.ts
+++ better-analytics-dashboard/prisma/seed.ts
@@ -3,12 +3,47 @@ import { Prisma, PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
+const SITE_ID = 'default-site';
+
 const siteData: Prisma.SiteCreateInput = {
-  siteId: 'default-site',
+  siteId: SITE_ID,
 }
 
+const funnelData = {
+  name: 'Basic funnel',
+  siteId: SITE_ID,
+  pages: [
+    '/dashboard',
+    '/dashboard/pages',
+    '/dashboard/geography',
+  ]
+}
+
+
 export async function main() {
-  await prisma.site.create({ data: siteData })
+  // Create site
+  const siteExists = await prisma.site.findFirst({
+    where: {
+      siteId: siteData.siteId
+    }
+  });
+
+  if (!siteExists) { 
+    await prisma.site.create({ data: siteData });
+  }
+
+  // Create funnel
+  const funnelExists = await prisma.funnel.findFirst({
+    where: {
+      name: funnelData.name
+    }
+  });
+
+  if (!funnelExists) {  
+    await prisma.funnel.create({
+      data: funnelData
+    });
+  }
 }
 
 main()
