117b8de68537310cc58582ca91ba73ff16ff0d46
Thomas <thomas.schauser@gmail.com>
2025-05-13 18:35:54 +0200

    Added funnel as BarChart

--- better-analytics-dashboard/src/app/dashboard/funnels/FunnelDataContent.tsx
+++ better-analytics-dashboard/src/app/dashboard/funnels/FunnelDataContent.tsx
@@ -2,6 +2,8 @@ import { fetchFunnelDetailsAction } from "@/app/actions/funnels";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Funnel, FunnelDetails } from "@/entities/funnels";
 import { useQuery } from "@tanstack/react-query";
+import { useMemo } from "react";
+import { Bar, BarChart, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
 
 type FunnelDataContentProps = {
   funnel: Funnel;
@@ -14,32 +16,31 @@ export function FunnelDataContent({ funnel }: FunnelDataContentProps) {
     queryFn: () => fetchFunnelDetailsAction('default-site', funnel.id),
   });
 
+  const chartData = useMemo(() => {
+    if (!funnelDetails) {
+      return [];
+    }
+
+    return funnelDetails.pages.map((page, index) => ({ page, visitors: funnelDetails.visitors[index] }));
+  }, [funnelDetails])
+
   if (funnelDetailsLoading || !funnelDetails) {
     return <div>Loading...</div>;
   }
 
+  console.log(chartData)
+
   return (
     <div>
-      <Table className="w-full">
-        <TableHeader>
-          <TableRow className="border-b">
-            <TableHead className="py-3 text-gray-700 font-medium">Page</TableHead>
-            <TableHead className="py-3 text-gray-700 font-medium text-right">Visitors</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {funnelDetails.pages.map((page, index) => (
-            <TableRow key={page} className="border-b last:border-b-0">
-              <TableCell className="py-4 font-medium text-gray-900">
-                {page}
-              </TableCell>
-              <TableCell className="py-4 text-gray-900 text-right">
-                {funnelDetails.visitors[index]}
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      <ResponsiveContainer width="100%" height={300}>
+        <BarChart layout="vertical" data={chartData} margin={{ top: 0, bottom: 0, left: 300, right: 500}}>
+          <XAxis type="number" />
+          <YAxis dataKey="page" type="category" />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="visitors" fill="#8884d8" />
+        </BarChart>
+      </ResponsiveContainer>
     </div>
   );
 }
