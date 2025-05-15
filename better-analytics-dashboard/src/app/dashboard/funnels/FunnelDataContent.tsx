b8057d11bffcd199b28b65eefa6668aa62a2ab7d
Thomas <thomas.schauser@gmail.com>
2025-05-15 20:09:06 +0200

    Improved funnel steps design

--- better-analytics-dashboard/src/app/dashboard/funnels/FunnelDataContent.tsx
+++ better-analytics-dashboard/src/app/dashboard/funnels/FunnelDataContent.tsx
@@ -41,27 +41,47 @@ export function FunnelDataContent({ funnel }: FunnelDataContentProps) {
     <div className="bg-white border-1 p-5 rounded-md">
       <div className="flex items-center gap-3 mb-3">
         <h1 className="text-xl font-semibold">{funnelDetails.name}</h1>
-        <Badge className="rounded-full h-[50%] mt-1" variant='outline'>{chartData.length} steps</Badge>  
+        <Badge className="rounded-full h-[50%] mt-1 text-gray-800" variant='outline'>{chartData.length} steps</Badge>  
       </div>
       <hr />
-      <div className="flex flex-col gap-6 mt-5">
-        {
-          chartData
-            .map(({ page, visitors }, index) => (
-              <div key={page} className="flex flex-col gap-3">
-                <div className="flex items-center gap-3">
-                  <p className="flex justify-center items-center bg-gray-200 size-6 rounded-full text-sm">{index+1}</p>
-                  <p className="font-semibold text-sm">{page}</p>
+      <div className="grid grid-cols-3">
+        <div className="col-span-2 text-sm font-semibold">
+          {
+            chartData
+              .map(({ page, visitors }, index) => (
+                <div className="mt-3" key={page}>
+                  <div className="flex items-end justify-between">
+                    <div className="flex items-center gap-3">
+                      <p className="flex justify-center items-center bg-gray-200 size-6 rounded-full font-medium text-xs">{index+1}</p>
+                      <p>{page}</p>
+                    </div>
+                    <p className="mt-3">{visitors} users</p>
+                  </div>
+                  <div className="p-3 text-gray-600">
+                    <Progress
+                      className="h-4"
+                      value={100 * visitors / maxVisitors}
+                      color="#22C55E"
+                    />
+                    <div className="flex justify-between items-end">
+                      <p>{visitors} users</p>
+                      <p className="text-right">{Math.floor(100 * visitors / maxVisitors)}%</p>
+                    </div>
+                    <Progress
+                      className="mt-2"
+                      value={100 - (100 * visitors / (chartData[index-1]?.visitors ?? maxVisitors))}
+                      color="#F97315"
+                    />
+                    <div className="flex justify-between items-end">
+                      <p>{(chartData[index-1]?.visitors ?? maxVisitors) - visitors} users dropped</p>
+                      <p className="text-right">{Math.floor(100 - (100 * visitors / (chartData[index-1]?.visitors ?? maxVisitors)))}%</p>
+                    </div>
+                  </div>
+                  <hr />
                 </div>
-                <p className="font-semibold text-sm">{visitors} users</p>
-                <Progress
-                  value={100 * visitors / maxVisitors}
-                  color="#22C55E"
-                />
-                <p className="text-right font-semibold text-sm">{Math.floor(100 * visitors / maxVisitors)}%</p>
-              </div>
-            ))
-        }
+              ))
+          }
+        </div>
       </div>
     </div>
   );
--- better-analytics-dashboard/src/components/ui/progress.tsx
+++ better-analytics-dashboard/src/components/ui/progress.tsx
@@ -8,6 +8,7 @@ import { cn } from "@/lib/utils"
 function Progress({
   className,
   value,
+  color,
   ...props
 }: React.ComponentProps<typeof ProgressPrimitive.Root>) {
   return (
@@ -22,7 +23,7 @@ function Progress({
       <ProgressPrimitive.Indicator
         data-slot="progress-indicator"
         className="h-full w-full flex-1 transition-all"
-        style={{ transform: `translateX(-${100 - (value || 0)}%)`, background: '#22C55E' }}
+        style={{ transform: `translateX(-${100 - (value || 0)}%)`, background: color }}
       />
     </ProgressPrimitive.Root>
   )
