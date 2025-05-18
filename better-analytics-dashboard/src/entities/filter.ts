c3df33b67c125af4997bfad26929a23824211ffe
Thomas <thomas.schauser@gmail.com>
2025-05-18 16:35:26 +0200

    Added query filter entity

---
+++ better-analytics-dashboard/src/entities/filter.ts
@@ -0,0 +1,34 @@
+import { z } from "zod";
+
+const FILTER_KINDS = [
+  "url",
+  "device_type",
+  "country_code",
+  "browser",
+  "os",
+  "referrer_source",
+  "referrer_source_name",
+  "referrer_search_term",
+  "referrer_url",
+  "utm_source",
+  "utm_medium",
+  "utm_campaign",
+  "utm_term",
+  "utm_content",
+  "event_type",
+  "custom_event_name"
+] as const;
+
+const FILTER_OPERATORS = [
+  "is",
+  "is-not",
+  "contains"
+] as const;
+
+export const QueryFilterSchema = z.object({
+  kind: z.enum(FILTER_KINDS),
+  operator: z.enum(FILTER_OPERATORS),
+  value: z.string()
+});
+
+export type QueryFilter = z.infer<typeof QueryFilterSchema>;
