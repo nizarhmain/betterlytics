c3010877ef7a1f45207cedfe4a9e208d005c12b7
Thomas <thomas.schauser@gmail.com>
2025-05-21 19:36:09 +0200

    Sidebar update

---
+++ better-analytics-dashboard/src/components/sidebar/BASidebar.tsx
@@ -0,0 +1,81 @@
+import Link from "next/link";
+import { LayoutDashboard, FileText, Smartphone, CircleDot, Globe, Link2, Funnel, DollarSign } from "lucide-react";
+import { BASidebarHeader } from "./BASidebarHeader";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem
+} from "@/components/ui/sidebar";
+import BASidebarCollapsibleSignOutButton from "./BASidebarCollapsibleSignOutButton";
+
+const navItems = [
+  { name: "Overview", href: "/dashboard", icon: <LayoutDashboard size={18} /> },
+  //{ name: "Realtime", href: "/dashboard/realtime", icon: <BarChart size={18} /> },
+  { name: "Pages", href: "/dashboard/pages", icon: <FileText size={18} /> },
+  { name: "Referrers", href: "/dashboard/referrers", icon: <Link2 size={18} /> },
+  //{ name: "Geography", href: "/dashboard/geography", icon: <Globe size={18} /> },
+  //{ name: "Referrers", href: "/dashboard/referrers", icon: <Link2 size={18} /> },
+  { name: "Geography", href: "/dashboard/geography", icon: <Globe size={18} /> },
+  { name: "User Journey", href: "/dashboard/user-journey", icon: <CircleDot size={18} /> },
+  { name: "Funnels", href: "/dashboard/funnels", icon: <Funnel size={18} /> },
+  { name: "Devices", href: "/dashboard/devices", icon: <Smartphone size={18} /> },
+  { name: "Campaigns", href: "/dashboard/campaign", icon: <DollarSign size={18} /> },
+  { name: "Events", href: "/dashboard/events", icon: <CircleDot size={18} /> },
+];
+
+export default function BASidebar() {
+  return (
+    <Sidebar variant="floating" collapsible="icon">
+      <SidebarHeader>
+        <BASidebarHeader />
+      </SidebarHeader>
+      <SidebarContent className="pl-1">
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {navItems.map((item) => (
+                  <SidebarMenuItem key={item.name}>
+                    <SidebarMenuButton asChild>
+                      <Link href={item.href}>
+                        <span>{item.icon}</span>
+                        <span>{item.name}</span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <BASidebarCollapsibleSignOutButton />
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+    // <aside className="w-56 bg-sidebar border-r border-border h-screen flex flex-col p-4">
+    //   <SidebarHeader />
+    //   <nav className="flex-1">
+    //     <div className="text-xs text-muted-foreground mb-2">Analytics</div>
+    //     <ul className="space-y-1">
+    //       {navItems.map((item) => (
+    //         <li key={item.name}>
+    //           <Link href={item.href} className="flex items-center gap-2 px-2 py-2 rounded hover:bg-accent hover:text-accent-foreground text-foreground text-sm font-medium">
+    //             <span>{item.icon}</span>
+    //             {item.name}
+    //           </Link>
+    //         </li>
+    //       ))}
+    //     </ul>
+    //   </nav>
+    // </aside>
+  );
+} 
