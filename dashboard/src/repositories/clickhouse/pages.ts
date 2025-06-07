f6aaf6b9c921000c6ce0186e283521c15e69aa21
Thomas <thomas.schauser@gmail.com>
2025-06-07 19:29:13 +0200

    Made dashboard compilable

--- dashboard/src/repositories/clickhouse/pages.ts
+++ dashboard/src/repositories/clickhouse/pages.ts
@@ -14,8 +14,6 @@ import {
   TopEntryPageRowSchema,
   TopExitPageRow,
   TopExitPageRowSchema,
-  DailyUniquePagesRow,
-  DailyUniquePagesRowSchema,
   DailyAverageTimeRow,
   DailyAverageTimeRowSchema,
   DailyBounceRateRow,
