3b45a2df7f792f407fd86febf692a5d8bf69c312
Thomas <thomas.schauser@gmail.com>
2025-06-19 15:53:54 +0200

    Merged migrations

--- dashboard/prisma/migrations/20250606140041_storing_query_filters_as_json_in_funnels/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Changed the type of `pages` on the `Funnel` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
-
-*/
--- AlterTable
-ALTER TABLE "Funnel" DROP COLUMN "pages",
-ADD COLUMN     "pages" JSONB NOT NULL;
