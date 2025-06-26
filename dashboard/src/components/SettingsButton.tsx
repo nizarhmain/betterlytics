61a9461cf3af29234c9c0485f5cfeb4cfd5556c3
Thomas <thomas.schauser@gmail.com>
2025-06-26 17:26:57 +0200

    Fixed actual issue

--- dashboard/src/components/SettingsButton.tsx
+++ dashboard/src/components/SettingsButton.tsx
@@ -1,10 +1,10 @@
-"use client";
+'use client';
 
 import SettingsPopover from './SettingsPopover';
 import { Settings } from 'lucide-react';
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import { useState } from 'react';
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 
 export default function SettingsButton() {
   const [isPopoverOpen, setIsPopoverOpen] = useState(false);
@@ -12,17 +12,17 @@ export default function SettingsButton() {
   return (
     <Popover open={isPopoverOpen} onOpenChange={setIsPopoverOpen}>
       <PopoverTrigger asChild>
-        <Button 
-          variant="ghost"
-          className="flex items-center gap-2 px-2 py-2 rounded hover:bg-accent hover:text-accent-foreground text-foreground text-sm font-medium w-full"
+        <Button
+          variant='ghost'
+          className='hover:bg-accent hover:text-accent-foreground text-foreground flex w-full items-center gap-2 rounded px-2 py-2 text-sm font-medium'
         >
           <Settings size={18} />
           Settings
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-auto p-0 border-none shadow-lg" side="top" align="start">
+      <PopoverContent className='!z-1200 w-auto border-none p-0 shadow-lg' side='top' align='start'>
         <SettingsPopover onClose={() => setIsPopoverOpen(false)} />
       </PopoverContent>
     </Popover>
   );
-} 
+}
